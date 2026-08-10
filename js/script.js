e1417350285373acdf3972cdf9c1ddf2929581d5
Ray-han25 <abdulllahmutazab@gmail.com>
2026-08-10 21:05:03 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// ------------------dom-------------
+const playerno1=document.querySelector(".playerno1")
+
+const handelNo1=()=>{
+   if(playerno1.value==""){
+    alert("Hello World!")
+   }else if(playerno1.value<0||playerno1.value>10){
+    alert("You write 0-10")
+   }else{
+   
+   }
+}
