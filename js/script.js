7da069e49a5c3006d6db4458832a6ef6ac17014d
Ray-han25 <abdulllahmutazab@gmail.com>
2026-08-11 08:38:27 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
 // ------------------dom-------------
-const playerno1=document.querySelector(".playerno1")
+const playerno1 = document.querySelector(".playerno1")
+const divOne = document.querySelector('.inputno1')
 
-const handelNo1=()=>{
-   if(playerno1.value==""){
-    alert("Hello World!")
-   }else if(playerno1.value<0||playerno1.value>10){
-    alert("You write 0-10")
+// -----------same--------------
+const h2 = document.querySelector('h2');
+const p = document.querySelector('p');
+
+// ---------------donTow--------
+const divTow = document.querySelector('.inputno2')
+const playerno2 = document.querySelector(".playerno2")
+
+
+
+const handelNo1 = () => {
+   if (playerno1.value == "") {
+      alert("Write something.")
+   } else if (playerno1.value < 0 || playerno1.value > 10) {
+      alert("You write 0-10")
+   } else {
+
+      divOne.style = 'display:none;'
+      divTow.style = 'display:flex;'
+      h2.innerHTML = "PLAYER NO. 2";
+   }
+}
+const handelNoTow=()=>{
+  
+if (playerno2.value == "") {
+      alert("Write something.")
+   } else if(playerno2.value<0||playerno2.value>10){
+      alert('you write somting')
+   }  else if(playerno1.value==playerno2.value){
+     p.innerHTML='The second player wins. 🏆'
    }else{
-   
+      p.innerHTML='The first player is winning. 🏆'
    }
 }
